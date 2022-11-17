ae82e688d379949a2fc0ee7958f984756f3a652c
Lyubo Kamenov <lyubo@meroxa.io>
2022-11-17 12:24:50 -0500

    Stub missing code for validation proto for ruby.
    The validation plugin does not support proto, however the generator
    will still emit a `require` for the the code. This change stubs the
    expected code which is a no-op. Proto validation in ruby is not supported
    
    remove proto and stub validation

---
+++ lib/ruby/turbine_rb/lib/validate/validate_pb.rb
@@ -0,0 +1,3 @@
+# This file is a place holder, do not remove.
+# The plugin used for `validate/validate.proto` does not support ruby, however
+# the `require` file in the emitted code references it even though it doesn't exist.
