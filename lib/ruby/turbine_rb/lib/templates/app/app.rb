b3dd02568f7196e9b43dbac67137ab40f7da55b9
Diana Doherty <diana@meroxa.io>
2022-12-01 17:13:22 -0500

    Add more comments around example ruby app

--- lib/ruby/turbine_rb/lib/templates/app/app.rb
+++ lib/ruby/turbine_rb/lib/templates/app/app.rb
@@ -6,48 +6,59 @@ require "turbine_rb"
 
 class MyApp
   def call(app)
+    # To configure resources for your production datastores
+    # on Meroxa, use the Dashboard, CLI, or Terraform Provider
+    # For more details refer to: http://docs.meroxa.com/
+    #
+    # Identify the upstream datastore with the `resource` function
+    # Replace `demopg` with the resource name configured on Meroxa
     database = app.resource(name: "demopg")
 
-    # ELT pipeline example
-    # records = database.records(collection: 'events')
-    # database.write(records: records, collection: 'events_copy')
+    # Specify which upstream records to pull
+    # with the `records` function
+    # Replace `collection_name` with a table, collection,
+    # or bucket name in your data store.
+    # If a configuration is needed for your source,
+    # you can pass it as a second argument to the `records` function. For example:
+    # database.records(collection: "collection_name", configs: {"incrementing.column.name" => "id"})
+    records = database.records(collection: "collection_name")
 
-    # procedural API
-    records = database.records(collection: "events")
+    # Register secrets to be available in the function:
+    # app.register_secrets("MY_ENV_TEST")
 
-    # This register the secret to be available in the turbine application
-    app.register_secrets("MY_ENV_TEST")
-
-    # you can also register several secrets at once
+    # Register several secrets at once:
     # app.register_secrets(["MY_ENV_TEST", "MY_OTHER_ENV_TEST"])
 
-    # Passthrough just has to match the signature
+    # Specify the code to execute against `records` with the `process` function.
+    # Replace `Passthrough` with your desired function.
+    # Ensure desired function matches `Passthrough`'s' function signature.
     processed_records = app.process(records: records, process: Passthrough.new)
-    database.write(records: processed_records, collection: "events_copy")
 
-    # out_records = processed_records.join(records, key: "user_id", window: 1.day) # stream joins
-
-    # chaining API
-    # database.records(collection: "events").
-    #   process_with(process: Passthrough.new).
-    #   write_to(resource: database, collection: "events_copy")
+    # Specify where to write records using the `write` function.
+    # Replace `collection_archive` with whatever data organisation method
+    # is relevant to the datastore (e.g., table, bucket, collection, etc.)
+    # If additional connector configs are needed, provided another argument. For example:
+    # database.write(
+    #   records: processed_records,
+    #   collection: "collection_archive",
+    #   configs: {"behavior.on.null.values": "ignore"})
+    database.write(records: processed_records, collection: "collection_archive")
   end
 end
 
-# might be useful to signal that this is a special Turbine call
 class Passthrough < TurbineRb::Process
   def call(records:)
     puts "got records: #{records}"
-    # to get the value of unformatted records, use record .value getter method
+    # To get the value of unformatted records, use record .value getter method
     # records.map { |r| puts r.value }
     #
-    # to transform unformatted records, use record .value setter method
+    # To transform unformatted records, use record .value setter method
     # records.map { |r| r.value = "newdata" }
     #
-    # to get the value of json formatted records, use record .get method
+    # To get the value of json formatted records, use record .get method
     # records.map { |r| puts r.get("message") }
     #
-    # to transform json formatted records, use record .set methods
+    # To transform json formatted records, use record .set methods
     # records.map { |r| r.set('message', 'goodbye') }
     records
   end
